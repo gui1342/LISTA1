e4177c8aab3fc659026fe7a23e9bed29d134bf0d
gui1342 <103466943+gui1342@users.noreply.github.com>
2022-07-14 20:47:54 -0300

    Update and rename QUESTÃO23 to QUESTÃO23.c

--- QUESTÃO23.c
+++ QUESTÃO23.c
@@ -1,4 +1,5 @@
-//23. Escreva um programa que receba a entrada de um número de três dígitos, separe o número em seus dígitos componentes e reconstrua um número com os componentes na ordem inversa. Exemplo: para entrada de 123, a resposta do programa seria 321.
+/*23. Escreva um programa que receba a entrada de um número de três dígitos, separe o número em seus dígitos 
+componentes e reconstrua um número com os componentes na ordem inversa. Exemplo: para entrada de 123, a resposta do programa seria 321.*/
 #include <stdio.h>
 
 int main() {

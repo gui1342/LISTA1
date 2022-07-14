cfb5eaeb45c009513db567627a6500acaab437a1
gui1342 <103466943+gui1342@users.noreply.github.com>
2022-07-14 20:41:31 -0300

    Update and rename QUESTÃO14 to QUESTÃO14.c

--- QUESTÃO14.c
+++ QUESTÃO14.c
@@ -1,4 +1,5 @@
-//14. Escreva um programa que leia uma temperatura em graus Celsius e apresente convertida em graus Fahrenheit. A fórmula de versão ́e F = (9 * C + 160) / 5, sendo F a temperatura Fahrenheit e C a temperatura em Celsius.
+/*14. Escreva um programa que leia uma temperatura em graus Celsius e apresente convertida em graus Fahrenheit. 
+A fórmula de versão ́e F = (9 * C + 160) / 5, sendo F a temperatura Fahrenheit e C a temperatura em Celsius.*/
 
 #include <stdio.h>
 
